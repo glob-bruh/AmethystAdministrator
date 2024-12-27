68e735a9eed9afd3fc62c79de8331db51de20c0e
glob-bruh <34488272+glob-bruh@users.noreply.github.com>
2024-12-26 17:51:45 -0700

    Add features

--- canvasController.py
+++ canvasController.py
@@ -1,14 +1,22 @@
 from tkinter import *
 import random
-from PIL import Image, ImageTk
+from matplotlib import font_manager
+from PIL import Image, ImageTk, ImageFont, ImageDraw
 
 class hostCanvas():
-    def __init__(self, c, src):
+    def __init__(self, c, ip, src):
         print("mesa host")
         self.canvas = c
         self.imgMoveData = {"x": 0, "y": 0, "item": None}
         i = f"image{random.randint(0, 9999999999)}"
-        image = ImageTk.PhotoImage(Image.open(f"pic/{src}.png"))
+        image = Image.open(f"resources/pic/devicePic/{src}.png")
+        x = ImageDraw.Draw(image)
+        file = font_manager.findfont('Noto Sans')
+        # font = ImageFont.truetype(<font-file>, <font-size>)
+        font = ImageFont.truetype(file, 20)
+        # x.text((x, y),"Sample Text",(r,g,b))
+        x.text((0, 0), ip, (255,255,255), font = font)
+        image = ImageTk.PhotoImage(image)
         setattr(c, i, image)
         c.create_image(50, 50, image = getattr(c, i), tags = ("movable"))
         c.tag_bind("movable", "<ButtonPress-1>", self.imgMoveStart)
@@ -31,3 +39,12 @@ class hostCanvas():
         self.canvas.move(self.imgMoveData["item"], delta_x, delta_y)
         self.imgMoveData["x"] = event.x
         self.imgMoveData["y"] = event.y
+
+def deviceToImg(device):
+    match device:
+        case "workstation":
+            return "wrkStn"
+        case "server":
+            return "server"
+        case _:
+            return "???"
--- main.py
+++ main.py
@@ -10,52 +10,64 @@ import canvasController as cc
 def addHost(c):
     global hostArray
     global ipAddr_txt
+    global hostType_drop_val
     global hosts_tree
-    x1 = cc.hostCanvas(c, "wrkStn")
+    x4 = cc.deviceToImg(hostType_drop_val.get())
+    if x4 == "???":
+        hostType_drop_val.set("workstation")
+        x4 = "wrkStn"
     x2 = ipAddr_txt.get()
+    x1 = cc.hostCanvas(c, x2, x4)
     x3 = hosts_tree.insert('', 'end', text = x2)
-    hostArray.append( [x2, x1, x3] )
+    hostArray.append( [x2, hostType_drop_val.get(), x1, x3] )
     ipAddr_txt.delete(0, END)
     print(hostArray)
 
 def removeHost():
     print("test")
 
-def initGui(tkDefaultWidth):
-    global windowMain
+def initGui(win, tkDefWidth):
     global ipAddr_txt
+    global hostType_drop_val
     global hosts_tree
-    windowMain = Tk()
     canvas = Canvas(
-        windowMain, 
+        win, 
         width=500, 
         height=500, 
         bg="#6b6b6b")
     hosts_tree = ttk.Treeview(
-        windowMain, 
+        win, 
         columns=('size', 'modified'))
     ipAddr_txt = Entry(
-        windowMain,
-        width = tkDefaultWidth,
+        win,
+        width = tkDefWidth,
         validate = 'key',)
+    hostType_drop_val = StringVar() 
+    hostType_drop_val.set( "Select Client Type" ) 
+    hostType_drop = OptionMenu(
+        win,
+        hostType_drop_val,
+        *[ "workstation", "server" ])  
     addHost_btn = Button(
-        windowMain,
-        width = tkDefaultWidth,
+        win,
+        width = tkDefWidth,
         text="Add Host",
         command=lambda: addHost(canvas))
     removeHost_btn = Button(
-        windowMain,
-        width = tkDefaultWidth,
+        win,
+        width = tkDefWidth,
         text="Remove Host",
         command=lambda: removeHost())
     canvas.grid(row = 1, column = 0, rowspan = 4)
     ipAddr_txt.grid(row = 1, column = 1)
-    addHost_btn.grid(row = 2, column = 1)
-    removeHost_btn.grid(row = 3, column = 1)
+    hostType_drop.grid(row = 2, column = 1)
+    addHost_btn.grid(row = 3, column = 1)
+    removeHost_btn.grid(row = 4, column = 1)
     hosts_tree.grid(row = 5, column = 0, columnspan = 2)
-    windowMain.title(f"{pgrmName}")
+    win.title(f"{pgrmName}")
 
 global pgrmName  ; pgrmName = "Armtiage Bootleg"
 global hostArray ; hostArray = []
-initGui(20)
+windowMain = Tk()
+initGui(windowMain, 20)
 windowMain.mainloop()
