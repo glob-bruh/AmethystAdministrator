446e4dbf03a2984ef844f7d2ee2904d1a7a700ac
SouthAlbertaAI <111461851+SouthAlbertaAI@users.noreply.github.com>
2024-12-29 15:56:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+-------------------------------------------------
+AMETHYST ADMINISTRATOR
+https://github.com/glob-bruh/AmthystAdministrator
+All content licensed under BSD-3
+-------------------------------------------------
+main - MAIN PYTHON FILE:
+-------------------------------------------------
+This file is the first thing executed. 
+-------------------------------------------------
+"""
+
 from main_window import MainWindow
 
 if __name__ == "__main__":
