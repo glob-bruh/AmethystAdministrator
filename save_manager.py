510de56d7d418ed15d5e42a7fc99168897d0397c
Lord Paramount <111461851+SouthAlbertaAI@users.noreply.github.com>
2025-01-27 19:44:43 -0700

    Updates to save manager, not committing other test files/modified other files for test

--- save_manager.py
+++ save_manager.py
@@ -1,11 +1,38 @@
 from main_window import MainWindow
-from networkPinger import pinger, icmpPingSend
-from servicesManager import ServiceManagementWindow, ServiceManagementAddServiceWindow
-from editHost import EditHostWindow
-from canvasController import hostCanvas
-import pyarrow.feather as feather
+import pickle
+import structlog as sl
+
+logger = sl.get_logger()
 
 
-class SaveManager:
-    def __int__(self):
-        self.skeleton_for_now = None
+def Save(mw: MainWindow) -> MainWindow:
+    """
+    :param mw: The MainWindow object from the application.
+    :return: The same MainWindow object being returned after hosts array saved.
+    """
+    try:
+        with open(".sacred/main_window.pickle", "wb") as pickle_management:
+            pickle.dump(mw.host_array, pickle_management, pickle.HIGHEST_PROTOCOL)
+    except Exception as e:
+        logger.error(f"Error In Save Manager Save: {e}")
+        raise e
+    return mw
+
+
+def Load(mw: MainWindow) -> MainWindow:
+    try:
+        with open(".sacred/main_window.pickle", "rb") as pickle_management:
+            mw.host_array = pickle.load(pickle_management)
+    except Exception as e:
+        logger.error(f"Error In Save Manager Load: {e}")
+        raise e
+    return mw
+
+
+def Delete(mw: MainWindow) -> MainWindow:
+    try:
+        mw.host_array = None
+    except Exception as e:
+        logger.error(f"Error In Save Manager Delete: {e}")
+        raise e
+    return mw
